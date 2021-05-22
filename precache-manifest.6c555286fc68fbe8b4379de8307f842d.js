db8b8da0da9ee57f019149c5984781e3303f2fd6
nikarageorgos <karageorgosni@gmail.com>
2021-05-22 16:03:20 -0400

    Updates

---
+++ precache-manifest.6c555286fc68fbe8b4379de8307f842d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3351d3ee136bc75072afba43ef26b2a8",
+    "url": "/auction-app/index.html"
+  },
+  {
+    "revision": "f323e90904f9aee36c52",
+    "url": "/auction-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a3a0a45d243b7f8a6c45",
+    "url": "/auction-app/static/js/2.d94943f2.chunk.js"
+  },
+  {
+    "revision": "f323e90904f9aee36c52",
+    "url": "/auction-app/static/js/main.b4fcae8b.chunk.js"
+  },
+  {
+    "revision": "f3bf5b93f7e7136135cd",
+    "url": "/auction-app/static/js/runtime-main.ad8c3a94.js"
+  }
+]);
